44775373487c4ab8f926b3a51da2af5078916896
jtq@1036012350 <JTQc5235>
2017-07-18 12:12:36 +0800

    SwipeDismissBehavior版本，好像最大高度被限制了。。。

--- app/src/main/java/com/marsjiang/myflipperbottomdialog/bottomsheet/BottomSheetFragment.java
+++ app/src/main/java/com/marsjiang/myflipperbottomdialog/bottomsheet/BottomSheetFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.SwipeDismissBehavior;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -30,6 +31,8 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     private static LinearLayout mll_bottom;
     private BottomSheetBehavior mBehavior;
 
+    private SwipeDismissBehavior swipeDismissBehavior;
+
     public static BottomSheetFragment getInstance(Context context, LinearLayout ll_bottom) {
         mContext = context;
         mll_bottom = ll_bottom;
@@ -44,14 +47,23 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         View ll_bottom_sheet_layout = inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
         //设定弹出评论区域高度
         ll_bottom_sheet = (CoordinatorLayout) ll_bottom_sheet_layout.findViewById(R.id.ll_bottom_sheet);
-//        RecyclerView rv_test = (RecyclerView) ll_bottom_sheet_layout.findViewById(R.id.rv_test);
-        int a = mll_bottom.getWidth();
-        int da = mll_bottom.getHeight();
-        CoordinatorLayout.LayoutParams layoutParams = new CoordinatorLayout.LayoutParams(a, da);
-        BottomSheetBehavior bottomSheetBehavior = new BottomSheetBehavior();
-        bottomSheetBehavior.setPeekHeight(da);
-        bottomSheetBehavior.setHideable(true);
-        layoutParams.setBehavior(bottomSheetBehavior);
+        RecyclerView rv_test = (RecyclerView) ll_bottom_sheet_layout.findViewById(R.id.rv_test);
+        int width = mll_bottom.getWidth();
+        int height = mll_bottom.getHeight();
+        CoordinatorLayout.LayoutParams layoutParams = new CoordinatorLayout.LayoutParams(width, height);
+//        BottomSheetBehavior bottomSheetBehavior = new BottomSheetBehavior();
+//        bottomSheetBehavior.setPeekHeight(da);
+//        bottomSheetBehavior.setHideable(true);
+        rv_test.setLayoutParams(layoutParams);
+        swipeDismissBehavior = new SwipeDismissBehavior();
+        swipeDismissBehavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY);
+
+        swipeDismissBehavior.setDragDismissDistance(0.5F);
+        swipeDismissBehavior.setStartAlphaSwipeDistance(0F);
+        swipeDismissBehavior.setEndAlphaSwipeDistance(0.5F);
+        swipeDismissBehavior.setSensitivity(0);
+        swipeDismissBehavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_START_TO_END);
+        layoutParams.setBehavior(swipeDismissBehavior);
         ll_bottom_sheet.setLayoutParams(layoutParams);
 
         return ll_bottom_sheet_layout;
@@ -83,7 +95,7 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
 
         @Override
         public int getItemCount() {
-            return 20;
+            return 50;
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
